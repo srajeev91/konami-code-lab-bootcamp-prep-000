0a790d3b18a872d7835908d2306faf6f7581f882
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-31 04:30:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let body = document.body;
+  let index = 0;
   
     body.addEventListener('keydown', function(e) {
-      if (e === code){
+      if (e === code[index]){
+        index++;
+      } if (index === code.length) {
       alert('Congrats, you did it!');
+      index = 0;
       }
-      
+      else {
+        index = 0;
+      }
     });
       
 }
