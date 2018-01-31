82b2c4c6840fa69f8beeffc1ebc70fd83355da9d
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-31 04:37:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   let index = 0;
   
     body.addEventListener('keydown', function(e) {
-      if (e.detail === parseInt(code[index])){
+      if (parseInt(e.detail) === code[index]){
         index++;
       } if (index === code.length) {
       alert('Congrats, you did it!');
